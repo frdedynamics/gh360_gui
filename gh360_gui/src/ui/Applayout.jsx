1c59cc7f91637990c9029fc4d8a65ceefaf4595f
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-03-17 13:04:28 +0100

    Small tweaks in the global.css file

--- gh360_gui/src/ui/Applayout.jsx
+++ gh360_gui/src/ui/Applayout.jsx
@@ -5,19 +5,14 @@ import Topbar from "./Topbar";
 function Applayout() {
   return (
     //  Create a page layout that is divided in two rows and two columns.
-    <div className="grid min-h-dvh grid-cols-[260px_1fr] grid-rows-[56px_1fr]">
+    <div className="grid min-h-dvh grid-cols-[260px_1fr]">
       {/* //Sidebar spans 2 rows, and leaves the remaining space to the maincontent + topbar*/}
-      <aside className="row-span-2 bg-background overflow-y-auto">
+      <aside className="bg-nav overflow-y-auto">
         <Navbar />
       </aside>
 
-      {/* Topbar  takes 56px and leaves the remaining space for the maincontent*/}
-      <header className="bg-background  px-4 flex items-center">
-        <Topbar />
-      </header>
-
       {/* Main content */}
-      <main className="min-w-0 overflow-y-auto bg-blue-300 h-full">
+      <main className="min-w-0 overflow-y-auto bg-background h-full">
         <Outlet />
       </main>
     </div>
