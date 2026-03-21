2499ee396be9f17ff64da38b31181c53ccd2e268
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-03-21 13:36:16 +0100

    Able to display the joint angle now, and connect to the middleware. Now possible to subscribe to different topics using a custom hook. Connection state is managed by a store (rosStore.js) and mounted in App.jsx

---
+++ gh360_gui/src/hooks/useTopics.js
@@ -0,0 +1,33 @@
+//The purpose of this class is to let the componenents subscribe to differente topics
+//Wrapped in a custom hook for
+
+import useRosStore from "@/store/rosStore";
+import { useEffect, useState } from "react";
+import * as ROSLIB from "roslib";
+
+const useTopics = (topicName, messageType, throttleRate = 100) => {
+  const ros = useRosStore((state) => state.ros);
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    console.log("ros is here? ", ros);
+    //If there is no ros, return immediately
+    if (!ros) return;
+
+    const topic = new ROSLIB.Topic({
+      ros,
+      name: topicName,
+      messageType,
+      throttle_rate: throttleRate,
+    });
+
+    topic.subscribe((msg) => setMessage(msg));
+
+    //When the component unmounts or a disconnect
+    return () => topic.unsubscribe();
+  }, [ros, topicName, messageType]);
+
+  return message;
+};
+
+export default useTopics;
--- gh360_gui/src/pages/Dashboard.jsx
+++ gh360_gui/src/pages/Dashboard.jsx
@@ -1,7 +1,33 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import useTopics from "@/hooks/useTopics";
+
 function Dashboard() {
+  const message = useTopics(
+    "/gh360/joint_states",
+    "sensor_msgs/msg/JointState",
+  );
+
   return (
-    <div className="flex justify-center items-center h-full text-6xl text-foreground">
-      Dashboard
+    <div className="flex justify-center items-center h-full  text-foreground">
+      <Card>
+        <CardHeader>
+          <CardTitle>Elbow Joint</CardTitle>
+          <CardDescription>This card shows the elbow joint</CardDescription>
+        </CardHeader>
+        <CardContent className="overflow-auto">
+          {!message ? (
+            <p>Waiting for data...</p>
+          ) : (
+            <p>{message.position.toString()}</p>
+          )}
+        </CardContent>
+      </Card>
     </div>
   );
 }
