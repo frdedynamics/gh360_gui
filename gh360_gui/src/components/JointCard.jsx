cc28ca289ca30c0b8325c40103c70ec5a0d5cd16
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-03-21 22:57:58 +0100

    Minor UI tweaks to make the dashboard look cleaner. Able to toggle between a more descriptive joint data

--- gh360_gui/src/components/JointCard.jsx
+++ gh360_gui/src/components/JointCard.jsx
@@ -3,9 +3,12 @@ import {
   Card,
   CardAction,
   CardContent,
+  CardDescription,
   CardHeader,
   CardTitle,
 } from "./ui/card";
+import { Minus, Plus } from "lucide-react";
+import { useState } from "react";
 
 function JointCard({ jointName, index, motors }) {
   //subscribes to  a joint state topic using the custom hook
@@ -21,25 +24,37 @@ function JointCard({ jointName, index, motors }) {
     "gh360_interfaces/msg/PortStatus",
   );
 
+  const [moreInfo, setMoreInfo] = useState(false);
+
   return (
-    <Card>
-      <CardHeader className="text-lg">
+    <Card className="w-full min-h-56 overflow-hidden">
+      <CardHeader className="text">
         <CardTitle>{jointName.replace("_", " ")}</CardTitle>
         <CardAction>
+          <button
+            className="cursor-pointer"
+            onClick={() => setMoreInfo((prev) => !prev)}
+          >
+            {!moreInfo ? (
+              <Plus
+                className=" hover:rotate-90 hover:duration-300 bg-gray-200 rounded-2xl"
+                strokeWidth={2.5}
+              />
+            ) : (
+              <Minus className=" bg-gray-200 rounded-2xl" strokeWidth={2.5} />
+            )}
+          </button>
+        </CardAction>
+        <CardDescription>
+          {" "}
           {!jointMessage ? (
             <p>Waiting for data...</p>
           ) : (
-            jointMessage.position[index].toFixed(3)
+            <p>Joint angle : {jointMessage.position[index].toFixed(3)} </p>
           )}
-        </CardAction>
+        </CardDescription>
       </CardHeader>
-      <CardContent>
-        {!jointMessage ? (
-          <p>Waiting for data...</p>
-        ) : (
-          <p>Joint angle : {jointMessage.position[index].toFixed(3)} </p>
-        )}
-
+      <CardContent className="overflow-y-auto ">
         {!motorMessage ? (
           <p>Waiting...</p>
         ) : (
@@ -48,16 +63,30 @@ function JointCard({ jointName, index, motors }) {
               <h2 className="font-semibold">Motor {motorIndex + 1}</h2>
               <p className="ml-5">
                 Present position:{" "}
-                {motorMessage.motors[motorIndex].present_position.toFixed(4)}
+                {motorMessage.motors[motorIndex].present_position.toFixed(3)}
               </p>
-              <p className="ml-5">
-                Present velocity:{" "}
-                {motorMessage.motors[motorIndex].present_velocity.toFixed(4)}
-              </p>
-              <p className="ml-5">
-                Present current:{" "}
-                {motorMessage.motors[motorIndex].present_current.toFixed(4)}
-              </p>
+
+              <div>
+                {moreInfo ? (
+                  <div>
+                    {" "}
+                    <p className="ml-5">
+                      Present velocity:{" "}
+                      {motorMessage.motors[motorIndex].present_velocity.toFixed(
+                        3,
+                      )}
+                    </p>
+                    <p className="ml-5">
+                      Present current:{" "}
+                      {motorMessage.motors[motorIndex].present_current.toFixed(
+                        3,
+                      )}
+                    </p>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
             </div>
           ))
         )}
--- gh360_gui/src/pages/Dashboard.jsx
+++ gh360_gui/src/pages/Dashboard.jsx
@@ -1,17 +1,36 @@
+import Camera from "@/components/Camera";
 import JointCard from "@/components/JointCard";
+import Model from "@/components/Model";
+import { Card, CardDescription, CardHeader } from "@/components/ui/card";
 import { JOINT_CONFIG } from "@/configs/jointConfigs";
 
 function Dashboard() {
   return (
-    <div className="grid grid-cols-3 gap-4 p-4">
-      {JOINT_CONFIG.map((joint) => (
-        <JointCard
-          key={joint.jointName}
-          jointName={joint.jointName}
-          index={joint.index}
-          motors={joint.motors}
-        />
-      ))}
+    <div className="grid grid-cols-2 w-full ">
+      {/* LEFT side of the dashboard */}
+      <div className="grid grid-cols-2 gap-4 items-start w-full p-4">
+        {/* An info card for other things to display */}
+        {/* !TODO */}
+        <Card className="min-h-56">
+          <CardHeader></CardHeader>
+          <CardDescription></CardDescription>
+        </Card>
+        {JOINT_CONFIG.map((joint) => (
+          <div key={joint.jointName} className="w-full">
+            <JointCard
+              jointName={joint.jointName}
+              index={joint.index}
+              motors={joint.motors}
+            />
+          </div>
+        ))}
+      </div>
+
+      {/* RIGHT SIDE OF THE DASHBOARD */}
+      <aside className="grid grid-rows-2 items-center justify-center min-h-screen">
+        <Camera />
+        <Model />
+      </aside>
     </div>
   );
 }
