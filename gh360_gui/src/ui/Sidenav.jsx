27f3bf7a46f9e4deebc2c65e5c6acdcbdc846a8d
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-04-04 21:44:03 +0200

    small changes to the sidenav

--- gh360_gui/src/ui/Sidenav.jsx
+++ gh360_gui/src/ui/Sidenav.jsx
@@ -1,18 +1,31 @@
-import { Button } from "@/components/ui/button";
 import useRosStore from "@/store/rosStore";
+import { CircleQuestionMark, Code, LayoutDashboard } from "lucide-react";
 
 function Navbar() {
   const { status } = useRosStore();
 
+  const navbarOptions = [
+    { icon: <LayoutDashboard />, name: "Dashboard" },
+    { icon: <CircleQuestionMark />, name: "Placeholder" },
+    { icon: <Code />, name: "Block Programming" },
+  ];
+
   return (
-    <nav className="flex flex-col items-center h-full w-full p-4 text-foreground">
-      <div className="text-4xl">GH360 Robot Arm</div>
-      <div className="flex flex-col gap-20 p-20">
-        <Button className="bg-accent-foreground p-8">Dashboard</Button>
-        <Button className="bg-accent-foreground p-8">Camera</Button>
-        <Button className="bg-accent-foreground p-8">3D Model</Button>
+    <nav className="flex flex-col h-full w-full  ">
+      <div className="text-xl items-center justify-center font-semibold p4- p-2">
+        GH360 Robot Arm
       </div>
-      <h2 className="font-bold text-2xl text-white">Status: {status}</h2>
+      <div className="flex flex-col gap-5 mt-2">
+        {navbarOptions.map((item) => (
+          <button
+            className="flex gap-2 border-b-2 border-b-primary cursor-pointer hover:scale-105"
+            key={item.name}
+          >
+            <div className="ml-2">{item.icon}</div>
+            <div className="">{item.name}</div>
+          </button>
+        ))}
+      </div>
     </nav>
   );
 }
