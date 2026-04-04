e65947a06cba66af3bca1f16b386cfc75b9d3ba7
DAVNGU99 <david.h.nguyen1999@gmail.com>
2026-04-04 23:38:59 +0200

    dark mode and light mode now available, connection status on navbar is more defined

--- gh360_gui/src/components/JointCard.jsx
+++ gh360_gui/src/components/JointCard.jsx
@@ -1,26 +1,30 @@
-import { useRef, useState } from "react"
-import { Card, CardAction, CardContent, CardDescription, CardHeader, CardTitle } from "./ui/card"
-import { Minus, Plus } from "lucide-react"
-import useRosTopic from "@/hooks/useRosTopics"
+import { useRef, useState } from "react";
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Minus, Plus } from "lucide-react";
+import useRosTopic from "@/hooks/useRosTopics";
 
 function JointCard({ jointName, index, motors }) {
-  const [jointAngle, setJointAngle] = useState(null)
-  const [motorData, setMotorData] = useState(null)
-  const [moreInfo, setMoreInfo] = useState(false)
+  const [jointAngle, setJointAngle] = useState(null);
+  const [motorData, setMotorData] = useState(null);
+  const [moreInfo, setMoreInfo] = useState(false);
 
-  useRosTopic(
-    "/gh360/joint_states",
-    "sensor_msgs/msg/JointState",
-    100,
-    (msg) => setJointAngle(msg.position[index])
-  )
+  useRosTopic("/gh360/joint_states", "sensor_msgs/msg/JointState", 100, (msg) =>
+    setJointAngle(msg.position[index]),
+  );
 
   useRosTopic(
     "/gh360/motor_states_sorted",
     "gh360_interfaces/msg/PortStatus",
     100,
-    (msg) => setMotorData(motors.map((i) => msg.motors[i]))
-  )
+    (msg) => setMotorData(motors.map((i) => msg.motors[i])),
+  );
 
   return (
     <Card className="w-full h-full overflow-hidden flex flex-col">
@@ -32,9 +36,12 @@ function JointCard({ jointName, index, motors }) {
             onClick={() => setMoreInfo((prev) => !prev)}
           >
             {!moreInfo ? (
-              <Plus className="hover:rotate-90 hover:duration-300 bg-gray-200 rounded-2xl" strokeWidth={2.5} />
+              <Plus
+                className="hover:rotate-90 hover:duration-300 bg-muted rounded-2xl"
+                strokeWidth={2.5}
+              />
             ) : (
-              <Minus className="bg-gray-200 rounded-2xl" strokeWidth={2.5} />
+              <Minus className="bg-muted rounded-2xl" strokeWidth={2.5} />
             )}
           </button>
         </CardAction>
@@ -53,11 +60,17 @@ function JointCard({ jointName, index, motors }) {
           motorData.map((motor, i) => (
             <div key={motors[i]} className="mt-2">
               <p className="font-semibold text-sm">Motor {motors[i] + 1}</p>
-              <p className="ml-5">Present position: {motor.present_position.toFixed(3)}</p>
+              <p className="ml-5">
+                Present position: {motor.present_position.toFixed(3)}
+              </p>
               {moreInfo && (
                 <div>
-                  <p className="ml-5">Present velocity: {motor.present_velocity.toFixed(3)}</p>
-                  <p className="ml-5">Present current: {motor.present_current.toFixed(3)}</p>
+                  <p className="ml-5">
+                    Present velocity: {motor.present_velocity.toFixed(3)}
+                  </p>
+                  <p className="ml-5">
+                    Present current: {motor.present_current.toFixed(3)}
+                  </p>
                 </div>
               )}
             </div>
@@ -65,7 +78,7 @@ function JointCard({ jointName, index, motors }) {
         )}
       </CardContent>
     </Card>
-  )
+  );
 }
 
-export default JointCard
+export default JointCard;
--- gh360_gui/src/ui/Sidenav.jsx
+++ gh360_gui/src/ui/Sidenav.jsx
@@ -1,9 +1,11 @@
+import { Card } from "@/components/ui/card";
+import ModeToggle from "@/components/ui/modeToggle";
 import useRosStore from "@/store/rosStore";
 import { CircleQuestionMark, Code, LayoutDashboard } from "lucide-react";
 
 function Navbar() {
   const { status } = useRosStore();
-
+  const isConnected = status === "connected";
   const navbarOptions = [
     { icon: <LayoutDashboard />, name: "Dashboard" },
     { icon: <CircleQuestionMark />, name: "Placeholder" },
@@ -16,6 +18,7 @@ function Navbar() {
         GH360 Robot Arm
       </div>
       <div className="flex flex-col gap-5 mt-2">
+        {/* SIDENAV OPTIONS */}
         {navbarOptions.map((item) => (
           <button
             className="flex gap-2 border-b-2 border-b-primary cursor-pointer hover:scale-105"
@@ -25,6 +28,26 @@ function Navbar() {
             <div className="">{item.name}</div>
           </button>
         ))}
+      </div>
+
+      {/* CONNECTION STATUS AND THEME TOGGLER */}
+      <div className="mt-auto flex flex-col gap-2 p-2 border-t-2 border-t-primary">
+        <div className="flex items-center gap-2">
+          <div className="relative flex h-2 w-2">
+            <span
+              className={`animate-ping  absolute inline-flex h-full w-full rounded-full opacity-75 ${isConnected ? "bg-green-400" : "bg-red-400"}`}
+            />
+            <span
+              className={`relative inline-flex h-2 w-2 rounded-full ${isConnected ? "bg-green-500" : "bg-red-500"}`}
+            />
+          </div>
+          <p
+            className={`capitalize text-sm font-medium ${isConnected ? "text-green-500" : "text-red-500"}`}
+          >
+            {status}
+          </p>
+        </div>
+        <ModeToggle />
       </div>
     </nav>
   );
